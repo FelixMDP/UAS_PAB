6e598daf7fafeaef85469fe44f3b11307fb2e6d4
Radja Juang Jamemiko <rjboy556@gmail.com>
2025-05-10 13:35:45 +0700

    update

--- frkthreads/macos/Flutter/GeneratedPluginRegistrant.swift
+++ frkthreads/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -11,7 +11,7 @@ import firebase_auth
 import firebase_core
 import flutter_image_compress_macos
 import geolocator_apple
-import url_launcher_macos
+import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
@@ -20,5 +20,5 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FlutterImageCompressMacosPlugin.register(with: registry.registrar(forPlugin: "FlutterImageCompressMacosPlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
-  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
